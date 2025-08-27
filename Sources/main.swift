884e269d9da486107eb9f64f180b042a1c86d4ba
Alex Yang <2892565+aycyang@users.noreply.github.com>
2025-08-27 15:54:23 -0400

    Parse hex string to bytes using chunking iterator

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,27 +1,49 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-//for c in "abcdef".reversed().chunks(ofCount: 2) {
-//    print(c)
-//}
-
-struct ChunkedIterator: IteratorProtocol {
-    let innerIterator: any IteratorProtocol
-
-    init(_ inner: any IteratorProtocol) {
-        self.innerIterator = inner
+struct ChunkingIterator<T:IteratorProtocol>: IteratorProtocol {
+    private var wrapped: T
+    init(_ wrapped: T) {
+        self.wrapped = wrapped
     }
-
-    mutating func next() -> Element? {
-        return self.innerIterator.next();
-    }
-
-}
-
-extension Sequence {
-    func chunks(ofCount: Int) -> Int {
-        return ofCount
+    mutating func next() -> (T.Element, T.Element?)? {
+        let first = self.wrapped.next()
+        if first == nil {
+            return nil
+        }
+        return (first!, self.wrapped.next())
     }
 }
 
-print([1,2,3].chunks(ofCount: 2))
+enum MyError : Error {
+    case hexParseError
+}
+
+func hexCharToInt8(_ hexchar: Character) throws -> UInt8 {
+    switch hexchar {
+        case "0": return 0 case "1": return 1
+        case "2": return 2 case "3": return 3
+        case "4": return 4 case "5": return 5
+        case "6": return 6 case "7": return 7
+        case "8": return 8 case "9": return 9
+        case "a": return 10 case "b": return 11
+        case "c": return 12 case "d": return 13
+        case "e": return 14 case "f": return 15
+        default: throw MyError.hexParseError
+    }
+}
+
+func hexToBytesLE(_ hexstr: String) -> [UInt8] {
+    var it = ChunkingIterator(hexstr.reversed().makeIterator())
+    var cur = it.next()
+    var result: [UInt8] = []
+    while cur != nil {
+        var byte: UInt8 = try! hexCharToInt8(cur!.0)
+        if cur!.1 != nil {
+            let upper = try! hexCharToInt8(cur!.1!)
+            byte += upper << 4
+        }
+        result.append(byte)
+        cur = it.next()
+    }
+    return result
+}
+
+print(hexToBytesLE("badbeef"))
